fd209c674f0d4585a1a915b0d37c510f15411d09
Marius Räsener <931919+elmcrest@users.noreply.github.com>
2024-02-04 23:21:13 +0100

    WIP

---
+++ generator/src/main.rs
@@ -0,0 +1,22 @@
+extern crate tera;
+
+use std::io::prelude::*;
+
+use std::fs::File;
+use tera::{Context, Tera};
+
+fn main() {
+    let mut outfile = File::create("index.html").unwrap();
+    let mut buffer = Vec::new();
+    let context = Context::new();
+    // Use globbing
+    let tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    tera.render_to("base.html", &context, &mut buffer).unwrap();
+    outfile.write_all(&buffer).unwrap();
+}
